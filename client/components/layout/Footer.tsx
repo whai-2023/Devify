4058f6d8ce01405b322af41ea7a3f0879ca84476
jiho-burgesskim <128258002+jiho-burgesskim@users.noreply.github.com>
2023-06-29 20:45:22 +1200

    starting of footer

---
+++ client/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="flex">
+        <h1>DEVIFY</h1>
+        <div>box 1</div>
+        <div>box 2</div>
+        <div>box 3</div>
+      </div>
+    </footer>
+  )
+}
